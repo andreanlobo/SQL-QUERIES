2441b5f99e9b6cc454159288fc625dfbfd35b75d
Andrean Lobo <91346621+andreanlobo@users.noreply.github.com>
2023-01-22 23:15:21 +0100

    Update README.sql

--- README.sql
+++ README.sql
@@ -3,21 +3,17 @@ My SQL Queries
 
 -- All Valid Triplets That Can Represent a Country 
 
-Background: There are 3 schools in a certain place where each student is enrolled in only one school. Students need to be selected in a way that no 2 students share the same name and the same student_id. 
+-- Background: There are 3 schools in a certain place where each student is enrolled in only one school. Students need to be selected in a way that no 2 students share the same name and the same student_id. 
 
-Query:
+-- Query:
 
 SELECT schoola.student_name AS member_A,
        schoolb.student_name AS member_B,
        schoolc.student_name AS member_C
-FROM   schoola,
-       schoolb,
-       schoolc -- using a cross join to get all possible combinations
-WHERE
-  -- in the where condition do in such a way that no name from a specific column is repeated and neither the student_id
-  schoola.student_id <> schoolb.student_id
+FROM   schoola,schoolb,schoolc -- using a cross join to get all possible combinations
+WHERE schoola.student_id <> schoolb.student_id
   AND schoola.student_id <> schoolc.student_id
   AND schoolb.student_id <> schoolc.student_id
   AND schoola.student_name <> schoolb.student_name
   AND schoola.student_name <> schoolc.student_name
-  AND schoolb.student_name <> schoolc.student_name 
+  AND schoolb.student_name <> schoolc.student_name -- in the where condition do in such a way that no name from a specific column is repeated and neither the student_i
